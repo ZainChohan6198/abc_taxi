911bbbfd174e564f5b142ba90d8d1598e2783394
ZainChohan6198 <zain.chohan10@gmail.com>
2022-06-20 13:53:41 +0500

    Removed backend

--- backend2/a2btaxis/settings/local.py
+++
@@ -1,7 +0,0 @@
-from .base import *
-
-# ICABBI_API
-ICABBI_SERVER = 'https://api.icabbi.com/uk9/'
-ICABBI_USERNAME = '6b74d02d5d1248a3dbf94f89c112398171e285a5'
-ICABBI_PASSWORD = 'b72bc0114187d376fbe9ecd897d127b1b86bbb1c'
-
--- backend2/accounts/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from accounts.models import Contact, Display
-
-admin.site.register(Contact)
-
-admin.site.register(Display)
